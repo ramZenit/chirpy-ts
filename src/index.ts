3fd636288408d3ac0fd4aeeae14551e35d91b0f3
ramZenit <a.mb@outlook.it>
2025-12-01 12:34:55 +0100

    Add static file server serving on /app

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
-app.listen(8080);
+const PORT = 8080;
+
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerRediness);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+function handlerRediness(req: Request, res: Response) {
+  res.set("content-type", "text/plain; charset=utf-8").status(200).send("OK");
+}
