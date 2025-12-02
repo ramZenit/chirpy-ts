a424fc8278b65343eb60427592a431677bf9c963
ramZenit <a.mb@outlook.it>
2025-12-02 12:48:35 +0100

    Add chirp validation API endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {
 } from "./api/middleware.js";
 import { handlerReset } from "./api/reset.js";
 import { handlerMetrics } from "./api/metrics.js";
+import { handlerValidateChirp } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
@@ -13,9 +14,11 @@ const PORT = 8080;
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
 app.get("/admin/metrics", handlerMetrics);
+
 app.get("/api/healthz", handlerReadiness);
+app.post("/api/validate_chirp", handlerValidateChirp);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
